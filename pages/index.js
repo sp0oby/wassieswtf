0a5038d008a00d33c6f3054f149bd77de2b7eee5
spooby <39270883+sp0oby@users.noreply.github.com>
2021-08-20 11:39:26 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -90,7 +90,7 @@ export default function Home() {
               <h2 className="text-7xl text-center Poppitandfinch text-blau my-6">ROADMAP</h2>
               <ul className="">
                 <li className="text-xl text-white my-6  montserrat"><span className="font-bold"> 20% ETH Donation: </span> This donation will be made to <a target="_blank" href="https://www.autismspeaks.org/" className="underline text-blau font-bold">Autism Speaks</a>, who is working to create a kinder, <span className="font-bold">  </span> more inclusive world for people with autism.</li>
-                <li className="text-xl text-white my-6  montserrat"><span className="font-bold"> Wassie Holders Channel: </span> Holders of WASSIES will be able to join a special Discord chennel to access upcoming projects.</li>
+                <li className="text-xl text-white my-6  montserrat"><span className="font-bold"> Wassie Holders Channel: </span> Holders of WASSIES will be able to join a special Discord channel to access upcoming projects.</li>
                 <li className="text-xl text-white my-6  montserrat"><span className="font-bold"> Meme Contest: </span> WASSIES NFT holders will be eligible to participate in meme contest for WASSIES and ETHERUEM prizes.</li>
 
               </ul>

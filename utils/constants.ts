4055bd8094406c7874203fc053692d0615d845aa
spooby <39270883+sp0oby@users.noreply.github.com>
2021-09-10 21:19:19 -0400

    Add files via upload

---
+++ utils/constants.ts
@@ -0,0 +1,42 @@
+// Export selected default bags
+export const defaultBags = [
+  {
+    id: 4444,
+    attributes: [
+      "Finland",
+      'B1 Lancer',
+      "Panzer IV",
+      "Astute Class Submarine",
+      "Flying Wedge",
+      "Force Dispersal",
+      "Suicide Bomb",
+      "Utility Knife +1",
+    ],
+  },
+  {
+    id: 1111,
+    attributes: [
+      "Poland",
+      "Hawker Hurricane",
+      "Churchill Tank +2",
+      "Virginia Class Submarine",
+      "Radar Jamming",
+      "Spider Holes",
+      "Hydrogen Bomb",
+      "Technical Manuals",
+    ],
+  },
+  {
+    id: 5634,
+    attributes: [
+      "Canada",
+      "A-10 Thunderbolt II +2",
+      "Bob Semple Tank",
+      "Devonshire Battleship",
+      "Ambush",
+      "Foxholes",
+      "Dirty Bomb",
+      "Enemy Uniform +1"
+    ],
+  },
+];
